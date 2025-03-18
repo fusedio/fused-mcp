b384939f5838291b8c86de2b80f15b40cb2bde58
Maxime Lenormand <maximelenormand@maximes-mbp-2.home>
2025-03-18 18:55:36 +0100

    Updating to use latest commit

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
 
         udf_names = agents[0]["udfs"]
 
-    commit = "91c45fa"
+    commit = "5f860ab"
     mcp_utils = fused.load(
         f"https://github.com/fusedio/udfs/tree/{commit}/public/common_mcp"
     ).utils
